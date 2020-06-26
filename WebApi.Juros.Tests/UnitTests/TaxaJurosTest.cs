cd0a32fc1ea7087b8b43b09d70d37daea26d3b94
Leandro de Mello Fagundes <leandro@fagundes.email>
2020-06-25 21:14:03 -0300

    Melhorias nos testes e removidos mais alguns arquivos inesperados.

--- WebApi.Juros.Tests/UnitTests/TaxaJurosTest.cs
+++ WebApi.Juros.Tests/UnitTests/TaxaJurosTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-using WebApi.Juros.Controllers;
+﻿using System.Globalization;
 using WebApi.Juros.Domain.Exceptions;
 using WebApi.Juros.Domain.ValueObjects;
 using Xunit;
